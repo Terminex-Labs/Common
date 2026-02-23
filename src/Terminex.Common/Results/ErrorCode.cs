fcda201d6765cfa591941c881fc1b270161025df
texno stalin <texno.stalin@gmail.com>
2026-02-23 15:14:21 +0300

    feat: Реализовал паттерн Result

---
+++ src/Terminex.Common/Results/ErrorCode.cs
@@ -0,0 +1,84 @@
+﻿namespace Terminex.Common.Results
+{
+    //public enum ErrorCode
+    //{
+    //    None = 0,
+    //    Create = 1,
+    //    Delete = 2,
+    //    Update = 3,
+    //    NotFound = 4,
+    //    InvalidPassword = 5,
+    //    Server = 6,
+    //    Conflict = 7,
+    //    Save = 8,
+    //    Unauthorized = 9,
+    //    Validation = 10,
+    //    ValueObject = 11,
+    //    Redis = 12
+    //}
+
+    public readonly record struct ErrorCode : IEquatable<ErrorCode>
+    {
+        public readonly string Name { get; }
+        public readonly int Code { get; }
+
+        private ErrorCode(string name, int code)
+        {
+            Name = name;
+            Code = code;
+        }
+
+        // Ошибки не найденного элемента
+        public static readonly ErrorCode None = new(nameof(None), 0);
+        public static readonly ErrorCode Null = new(nameof(Null), 0);
+        public static readonly ErrorCode Empty = new(nameof(Empty), 0);
+
+        public static readonly ErrorCode Exist = new(nameof(Exist), 0);
+        public static readonly ErrorCode NotExist = new(nameof(NotExist), 0);
+        public static readonly ErrorCode NotFound = new(nameof(NotFound), 0);
+        public static readonly ErrorCode InvalidRequest = new(nameof(InvalidRequest), 0);
+        public static readonly ErrorCode InvalidResponse = new(nameof(InvalidResponse), 0);
+
+        // Серверные ошибки
+        public static readonly ErrorCode Create = new(nameof(Create), 0);
+        public static readonly ErrorCode Update = new(nameof(Update), 0);
+        public static readonly ErrorCode Delete = new(nameof(Delete), 0);
+        public static readonly ErrorCode Save = new(nameof(Save), 0);
+        public static readonly ErrorCode Conflict = new(nameof(Conflict), 0);
+        public static readonly ErrorCode Connection = new(nameof(Connection), 0);
+        public static readonly ErrorCode Server = new(nameof(Server), 0);
+
+        /// <summary>
+        /// Не корректный синтаксис или не праавильные параметры запроса
+        /// </summary>
+        public static readonly ErrorCode BadRequest = new(nameof(BadRequest), 0);
+
+        // Валидационные ошибки
+        public static readonly ErrorCode Validation = new(nameof(Validation), 0);
+
+        // Ошибки аутентификации и авторизации
+
+        /// <summary>
+        /// Аутентификация не пройдена, неверный пароль/логин, истекший или недействительный токен
+        /// </summary>
+        public static readonly ErrorCode Unauthorized = new(nameof(Unauthorized), 0);
+
+        /// <summary>
+        /// Пользователь аутентифицирован, но не имеет прав доступа к данному ресурсу (запрещено)
+        /// </summary>
+        public static readonly ErrorCode Forbidden = new(nameof(Forbidden), 0);
+
+        public static ErrorCode Custom(string name, int code)
+        {
+            if (code < 10000 && code > 29999)
+                throw new ArgumentOutOfRangeException(nameof(code), "Кастомная ошибка должна быть в диапозоне от 10000 до 29999.");
+
+            return new ErrorCode(name, code);
+        }
+
+        public static implicit operator string(ErrorCode errorCode) => errorCode.Name;
+        public static implicit operator int(ErrorCode errorCode) => errorCode.Code;
+
+        public override string ToString() => Code.ToString();
+    }
+}
---
+++ src/Terminex.Common/Results/Result.cs
@@ -0,0 +1,94 @@
+﻿using System.Text;
+
+namespace Terminex.Common.Results
+{
+    public class Result<TValue> : Result
+    {
+        private readonly TValue? _value;
+
+        private Result(TValue? value, bool isSuccess, List<Error> errors) : base(isSuccess, errors)
+        {
+            if (isSuccess && value is null && default(TValue) is not null)
+                throw new InvalidOperationException("Нельзя создать успешный результат для типа значения (value type), не допускающего null, с значением null.");
+
+            _value = value;
+        }
+
+        /// <exception cref="InvalidOperationException"></exception>
+        public TValue Value => IsSuccess ? _value! : throw new InvalidOperationException("Нельзя получить значение из неуспешного результата.");
+
+        /*--Фабричные методы------------------------------------------------------------------------------*/
+        public static Result<TValue> Success(TValue value) => new(value, true, []);
+
+        public new static Result<TValue> Failure(Error error) => new(default, false, [error]);
+
+        public new static Result<TValue> Failure(IEnumerable<Error> errors) => new(default, false, errors.ToList());
+
+        public TResult Match<TResult>(Func<TValue, TResult> onSuccess, Func<IReadOnlyList<Error>, TResult> onFailure) => IsSuccess ? onSuccess(Value) : onFailure(Errors);
+
+        public void Switch(Action<TValue> onSuccess, Action<IReadOnlyList<Error>> onFailure)
+        {
+            if (IsSuccess)
+                onSuccess(Value);
+            else
+                onFailure(Errors);
+        }
+    }
+
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+
+        public IReadOnlyList<Error> Errors { get; }
+
+        protected Result(bool isSuccess, List<Error> errors)
+        {
+            if (isSuccess && errors.Any())
+                throw new InvalidOperationException("Нельзя создать успешный результат с ошибками.");
+
+            if (!isSuccess && !errors.Any())
+                throw new InvalidOperationException("Нельзя создать провальный результат без ошибок.");
+
+            IsSuccess = isSuccess;
+            Errors = errors;
+        }
+
+        /*--Фабричные методы------------------------------------------------------------------------------*/
+        public static Result Success() => new(true, []);
+        public static Result Failure(Error error) => new(false, [error]);
+        public static Result Failure(IEnumerable<Error> errors) => new(false, errors.ToList());
+
+        public TResult Match<TResult>(Func<TResult> onSuccess, Func<IReadOnlyList<Error>, TResult> onFailure) => IsSuccess ? onSuccess() : onFailure(Errors);
+
+        public void Switch(Action onSuccess, Action<IReadOnlyList<Error>> onFailure)
+        {
+            if (IsSuccess)
+                onSuccess();
+            else
+                onFailure(Errors);
+        }
+
+        /*--Отображение ошибки в виде строки--------------------------------------------------------------*/
+        public string StringMessage => BuildMessage(error => $"Код: {(int)error.ErrorCode} - {error.ErrorCode}. Причина: {error.Message}");
+
+        private string BuildMessage(Func<Error, string> messageSelector)
+        {
+            if (Errors.Count == 0) return "Ошибок нет.";
+
+            var sb = new StringBuilder();
+            sb.AppendLine("Ошибки:");
+
+            for (int i = 0; i < Errors.Count; i++)
+            {
+                string message = messageSelector(Errors[i]);
+                sb.Append($"{i + 1}) ").AppendLine(message);
+            }
+
+            return sb.ToString();
+        }
+
+        /*--Переопределенные методы-----------------------------------------------------------------------*/
+        public override string ToString() => BuildMessage(error => $"Статус: [{IsSuccess}] Код: [{error.ErrorCode} - {(int)error.ErrorCode}] Информация: [{error.Message}]");
+    }
+}
